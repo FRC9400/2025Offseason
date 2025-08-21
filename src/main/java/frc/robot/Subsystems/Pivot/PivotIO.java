3f6e2bf3405aec305cdfc395692abb77f06d2157
josie-miller <josephinemiller13524@icloud.com>
2025-08-21 11:35:37 -0700

    pivot branch

--- src/main/java/frc/robot/Subsystems/Pivot/PivotIO.java
+++ src/main/java/frc/robot/Subsystems/Pivot/PivotIO.java
@@ -1,3 +1,19 @@
-public class PivotIO {
-    
+package frc.robot.Subsystems.Pivot;
+
+import org.littletonrobotics.junction.AutoLog;
+
+public interface PivotIO {
+    @AutoLog
+    public static class PivotIOInputs {
+        public double pivotAppliedVolts = 0.0;
+
+        public double pivotSetpointDeg = 0.0;
+        public double pivotSetpointRot = 0.0;
+        public double[] pivotPosDeg = new double[] {};
+        public double[] pivotPosRot = new double[] {};
+
+        public double[] pivotCurrent = new double[] {};
+        public double[] pivotTemp = new double[] {};
+        public double[] pivotRPS = new double[] {};
+    }
 }
--- src/main/java/frc/robot/Subsystems/Pivot/PivotIOTalonFX.java
+++ src/main/java/frc/robot/Subsystems/Pivot/PivotIOTalonFX.java
@@ -1,3 +1,25 @@
-public class PivotIOTalonFX {
-    
+package frc.robot.Subsystems.Pivot;
+
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+
+import frc.robot.Constants.canIDConstants;
+
+public class PivotIOTalonFX implements PivotIO {
+    /* Motor Objects */
+    private final TalonFX leftMotor1;
+    private final TalonFX leftMotor2;
+    private final TalonFX rightMotor1;
+    private final TalonFX rightMotor2;
+    private TalonFXConfiguration leftMotor1Configs;
+
+    public PivotIOTalonFX(){
+        /* Motor Objects */
+        leftMotor1 = new TalonFX(canIDConstants.leftPivotMotor1, "rio");
+        leftMotor2 = new TalonFX(canIDConstants.leftPivotMotor2, "rio");
+        rightMotor1 = new TalonFX(canIDConstants.rightPivotMotor1, "rio");
+        rightMotor2 = new TalonFX(canIDConstants.rightPivotMotor2, "rio");
+        leftMotor1Configs = new TalonFXConfiguration();
+    }
+
 }
