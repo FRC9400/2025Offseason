8305547241177f9166e4e6e50233aa5ee394b21c
josie-miller <josephinemiller13524@icloud.com>
2025-08-22 18:51:29 -0700

    new sys id

--- src/main/java/frc/robot/Subsystems/Pivot/Pivot.java
+++ src/main/java/frc/robot/Subsystems/Pivot/Pivot.java
@@ -19,9 +19,44 @@ import frc.robot.Constants.pivotConstants;
 public class Pivot extends SubsystemBase {
     private final PivotIO pivotIO;
     private PivotIOInputsAutoLogged inputs = new PivotIOInputsAutoLogged();
+    private final SysIdRoutine pivotRoutine;
+
 
     public Pivot(PivotIO pivotIO){
         this.pivotIO = pivotIO;
+        pivotRoutine = new SysIdRoutine(
+            new SysIdRoutine.Config(null, Volts.of(4), null,
+                    (state) -> SignalLogger.writeString("state", state.toString())),
+            new SysIdRoutine.Mechanism((volts) -> pivotIO.requestVoltage(volts.in(Volts)), null,
+                    this));
+    }
+
+    public Command pivotSysIdCmd(){
+        return Commands.sequence(
+            this.runOnce(() -> SignalLogger.start()),
+            pivotRoutine
+                    .quasistatic(Direction.kForward)
+                    .until(() -> Math.abs(inputs.positionDeg[0]) > 35), 
+            this.runOnce(() -> pivotIO.requestVoltage(0)),
+            Commands.waitSeconds(1),
+            pivotRoutine
+                    .quasistatic(Direction.kReverse)
+                    .until(() -> inputs.positionDeg[0] < 5), 
+            this.runOnce(() -> pivotIO.requestVoltage(0)),
+            Commands.waitSeconds(1),
+
+            pivotRoutine
+                    .dynamic(Direction.kForward)
+                    .until(() -> Math.abs(inputs.positionDeg[0]) > 35),
+            this.runOnce(() -> pivotIO.requestVoltage(0)),
+            Commands.waitSeconds(1),
+
+            pivotRoutine
+                    .dynamic(Direction.kReverse)
+                    .until(() -> inputs.positionDeg[0] < 5), //Keep in mind the max height is around 0.6
+            this.runOnce(() -> pivotIO.requestVoltage(0)),
+            Commands.waitSeconds(1),
+            this.runOnce(() -> SignalLogger.stop()));
     }
     
 
